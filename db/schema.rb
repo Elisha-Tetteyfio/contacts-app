2db4c8dcdc740b7c3f22cd7c45de0d53e616597a
Elisha <tetteyfioelisha@gmail.com>
2023-02-23 09:29:59 +0000

    Remove admin from users

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_123051) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
