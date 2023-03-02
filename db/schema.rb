703101e7a54cff97dabacc33bad29d81d2e80013
Elisha <tetteyfioelisha@gmail.com>
2023-03-02 14:08:32 +0000

    Generate cities scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_140048) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_140539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "region_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["region_id"], name: "index_cities_on_region_id"
+    t.index ["user_id"], name: "index_cities_on_user_id"
+  end
 
   create_table "regions", force: :cascade do |t|
     t.string "name"
@@ -39,5 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_140048) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cities", "regions"
+  add_foreign_key "cities", "users"
   add_foreign_key "regions", "users"
 end
