e1ff17363d267874fa263bc8dd943372f20a3a91
Elisha <tetteyfioelisha@gmail.com>
2023-03-02 14:27:31 +0000

    Setup associations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,5 @@
 class City < ApplicationRecord
   belongs_to :region
   belongs_to :user
+  has_many :suburbs
 end
--- app/models/suburb.rb
+++ app/models/suburb.rb
@@ -1,4 +1,5 @@
 class Suburb < ApplicationRecord
   belongs_to :city
   belongs_to :user
+  has_many :contacts
 end
