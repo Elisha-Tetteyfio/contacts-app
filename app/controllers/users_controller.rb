255f1261a0f1a31b1d383b75f0c87d027884477d
Elisha <tetteyfioelisha@gmail.com>
2023-03-12 21:26:35 +0000

    Style users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:fname, :lname, :phone, :birth_date)
+      params.require(:user).permit(:fname, :lname, :phone, :birth_date, :email, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,30 @@
 class User < ApplicationRecord
+  before_save :format_input
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :contacts
   has_one :user_role
-  has_many :roles, through: :user_role
+  has_one :role, through: :user_role
+
+  validates :fname, presence: true 
+  validates :phone, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 
   def super_admin?
-    roles.where(role_code: "SA").present?
+    # roles.where(role_code: "SA").present?
+    role.role_code == "SA"
   end
+
+  private
+
+  def format_input
+    self.fname = fname.strip.capitalize
+    self.lname = lname.strip.capitalize
+    self.phone = phone.strip
+    self.email = email.strip
+  end
+
 end
