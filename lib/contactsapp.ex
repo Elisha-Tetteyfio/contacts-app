fd464738ede37be2d17e2e58ad474798c8d70e57
Elisha-Tetteyfio <tetteyfioelisha@gmail.com>
2023-08-30 11:48:33 +0000

    Validation for delete contact

--- lib/contactsapp.ex
+++ lib/contactsapp.ex
@@ -119,16 +119,22 @@ defmodule Contactsapp do
 
   delete "/contacts/:id" do
     contact_id = conn.params["id"]
-
-    case Contact.delete_contact(contact_id) do
-      {:ok, _message} ->
+    case Validation.validate_id(contact_id) do
+      {:ok, _val} ->
+        case Contact.delete_contact(contact_id) do
+          {:ok, _message} ->
+            conn
+            |> put_resp_header("content-type", "application/json")
+            |> send_resp(200, Poison.encode!(%{resp_code: "00", message: "Contact successfully deleted"}))
+          {:notfound} ->
+            conn
+            |> put_resp_header("content-type", "application/json")
+            |> send_resp(404, Poison.encode!(%{resp_code: "01", message: "Contact does not exist"}))
+        end
+      {:error, reason} ->
         conn
         |> put_resp_header("content-type", "application/json")
-        |> send_resp(200, Poison.encode!(%{resp_code: "00", message: "Contact successfully deleted"}))
-      {:notfound} ->
-        conn
-        |> put_resp_header("content-type", "application/json")
-        |> send_resp(404, Poison.encode!(%{resp_code: "01", message: "Contact does not exist"}))
+        |> send_resp(404, Poison.encode!(reason))
     end
   end
 end
